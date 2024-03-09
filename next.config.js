30c1d355d1cddb3135ba553301267cb587afd124
yokomac <yokomaku.connecta@gmail.com>
2024-03-09 12:20:50 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: process.env.GITHUB_ACTIONS && "/blog",
+  output: "export",
   reactStrictMode: true,
+  trailingSlash: true,
+  images: { unoptimized: true } ,
+  
 }
-
-module.exports = nextConfig
+module.exports = nextConfig;
