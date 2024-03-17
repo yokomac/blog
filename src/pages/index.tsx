b338d1e6c88b08be09aa83f78c0d2ee9fd9868bc
yokomac <yokomaku.connecta@gmail.com>
2024-03-17 21:42:28 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,7 +39,6 @@ const Home: NextPage<Props> = ({ allPosts }) => {
         <h4>TAKUTO YOKOMAKU</h4>
         <p className={styles.description}>
           I am a web developer focusing on front-end development.
-          Always hungry to keep learning.
         </p>
         <div className="contact">
           <hr/>
