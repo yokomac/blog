30e359462a1088bb0ed9407ed70922fe60f1b0e7
yokomac <yokomaku.connecta@gmail.com>
2024-03-18 20:53:01 +0900

    fix

--- src/component/navigation.tsx
+++ src/component/navigation.tsx
@@ -2,15 +2,17 @@ import Image from 'next/image'
 import styles from "../styles/Home.module.css";
 import { basePath } from "../../next.config" // 追加
 
-const BASE_PATH = basePath ? basePath : "" // 追加
+type NavigationProps = {
+  basePath?: string;
+};
 
-const Navigation = () => (
+const Navigation: React.FC<NavigationProps> = ({ basePath = '' }) => (
   <div className={styles.navi}>
     <div className={styles.naviContent}>
       <a href="https://yokomac.github.io/blog/"> {/* リンク追加 */}
         <a>
           <Image
-            src={`${BASE_PATH}/next.svg`} // 修正
+            src={`${basePath}/next.svg`} // 修正
             alt="self"
             width={85}
             height={85}
@@ -27,7 +29,7 @@ const Navigation = () => (
       <hr/>
       <a href="https://github.com/yokomac/">
         <Image
-          src={`${BASE_PATH}/github-mark.svg`}
+          src={`${basePath}/github-mark.svg`}
           alt="github"
           width={20}
           height={20}
@@ -36,7 +38,7 @@ const Navigation = () => (
       </a>
       <a href="https://instagram.com/make_maku/">
         <Image
-          src={`${BASE_PATH}/instagram.svg`}
+          src={`${basePath}/instagram.svg`}
           alt="instagram"
           width={20}
           height={20}
@@ -45,7 +47,7 @@ const Navigation = () => (
       </a>
       <a href="https://x.com/maku_make/">
         <Image
-          src={`${BASE_PATH}/x-logo.svg`}
+          src={`${basePath}/x-logo.svg`}
           alt="x"
           width={20}
           height={20}
@@ -54,7 +56,7 @@ const Navigation = () => (
       </a>
       <a href="">
         <Image
-          src={`${BASE_PATH}/facebook.svg`}
+          src={`${basePath}/facebook.svg`}
           alt="facebook"
           width={20}
           height={20}
