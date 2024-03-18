0c2cb02ca0f04ef011ed572a435b127f9a270174
yokomac <yokomaku.connecta@gmail.com>
2024-03-18 21:14:46 +0900

    Update [slug].tsx

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -5,7 +5,10 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { getAllPosts, getPostBySlug } from "../lib/api";
 import markdownToHtml from "../lib/markdownToHtml";
+import Image from 'next/image'
 // import Navigation from '../component/navigation';
+import { basePath } from "../../next.config" // 追加
+const BASE_PATH = basePath ? basePath : "" // 追加
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
@@ -54,6 +57,62 @@ const Post: NextPage<Props> = ({ post }) => {
         <link rel="icon" href="/blog/favicon.ico" sizes="any" />
       </Head>
       <div className={styles.wrapper}>
+        <div className={styles.navi}>
+          <div className={styles.naviContent}>
+            <a href="https://yokomac.github.io/blog/">
+              <Image
+                src={`${BASE_PATH}/next.svg`} // 修正
+                alt="self"
+                width={85}
+                height={85}
+                className={styles.logo}
+              />
+            </a>
+            <h4 className={styles.name}>TAKUTO YOKOMAKU</h4>
+            <p className={styles.description}>
+              I am a web developer focusing on front-end development.
+            </p>
+          </div>
+          <div className="contact">
+            <hr/>
+            <a href="https://github.com/yokomac/">
+              <Image
+                src={`${BASE_PATH}/github-mark.svg`}
+                alt="github"
+                width={20}
+                height={20}
+                className={styles.logo}
+              />
+            </a>
+            <a href="https://instagram.com/make_maku/">
+              <Image
+                src={`${BASE_PATH}/instagram.svg`}
+                alt="instagram"
+                width={20}
+                height={20}
+                className={styles.logo}
+              />
+            </a>
+            <a href="https://x.com/maku_make/">
+              <Image
+                src={`${BASE_PATH}/x-logo.svg`}
+                alt="x"
+                width={20}
+                height={20}
+                className={styles.logo}
+              />
+            </a>
+            <a href="">
+              <Image
+                src={`${BASE_PATH}/facebook.svg`}
+                alt="facebook"
+                width={20}
+                height={20}
+                className={styles.logo}
+              />
+            </a>
+          </div>
+        </div>
         <main className={styles.main}>
           <article>
             <h1 className={styles.title}>{post.title}</h1>
